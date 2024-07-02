6f65e1bb8874c14c511cf40bb8bd5ba55e3957c9
datttrian <datttrian@gmail.com>
2024-07-02 19:22:21 +0000

    Tue, Jul 2, 2024, 7:22 PM +00:00

--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
-# Import the AutoTokenizer and AutoModel classes from transformers
-from transformers import AutoTokenizer, AutoModel
+# Import AutoModelForConditionalGeneration from transformers
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-# Load the pre-trained tokenizer of the "cardiffnlp/twitter-roberta-base-emoji" model
-tokenizer = AutoTokenizer.from_pretrained(
-    "cardiffnlp/twitter-roberta-base-emoji")
+# Load the pre-trained google/flan-t5-base model and tokenizer
+tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-base")
+model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-base")
 
-# Print the tokenizer to see the data preprocessing configuration for this model
-print(tokenizer)
+# Specify source and target languages for translation
+source_lang = "English"
+target_lang = "German"
+input_text = f"translate {source_lang} to {target_lang}: How old are you?"
 
-# Load the pre-trained "cardiffnlp/twitter-roberta-base-emoji" model
-model = AutoModel.from_pretrained("cardiffnlp/twitter-roberta-base-emoji")
+# Use the tokenizer to preprocess input text, with return_tensors="pt" to return PyTorch tensors
+inputs = tokenizer(input_text, return_tensors="pt")
 
-# Get the model configuration
-config = model.config
-
-# Print model config's architectures attribute to see the model class name
-print(model.config.architectures)
+# Print the key and values of the preprocessed inputs dictionary
+for key, value in inputs.items():
+    print(key, value)
